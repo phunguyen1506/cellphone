168477e4f8744d745b20ab4440abc1e4ffa4980d
phunguyen1506 <phungxuanthien15062001@gmail.com>
2023-05-26 22:53:50 +0700

    commit

--- config/db/db.js
+++ config/db/db.js
@@ -4,9 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 async function connectDB(){
-    // const url = 'mongodb://localhost:27017/shop'
-    // const url = 'mongodb+srv://CaoKhaHieu:<CaoKhaHieu>@cluster0.r9hva.mongodb.net/shop?retryWrites=true&w=majority'
-    const url = 'mongodb+srv://CaoKhaHieu:CaoKhaHieu@cluster0.5sfoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const url = 'mongodb://localhost:27017/cellphone'
     try {
         await mongoose.connect(url, {
             useNewUrlParser: true,
